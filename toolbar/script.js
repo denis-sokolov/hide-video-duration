10407202598678b4ec4c2acea8c7a539c40e4736
Denis Sokolov <denis@sokolov.cc>
2016-04-21 22:27:04 +0200

    Prefill rule field with the current video title

--- toolbar/script.js
+++ toolbar/script.js
@@ -2,6 +2,11 @@ chrome.tabs.insertCSS({
     code: '.ytp-progress-bar,.ytp-time-separator,.ytp-time-duration,.video-time{display:none!important}'
 });
 
+chrome.tabs.query({ active: true, currentWindow: true }, function(tabs){
+    var tab = tabs[0];
+    document.querySelector('.add-a-rule input').value =
+        tab.title.replace(/[\s-]+YouTube$/, '');
+});
 document.querySelector('.hide-automatically').addEventListener('click', function(){
     document.querySelector('.hide-success').style.display = 'none';
     document.querySelector('.add-a-rule').style.display = 'block';
