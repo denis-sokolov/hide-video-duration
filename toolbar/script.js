061ac824ecb4785d65a332c22ddc188bca0cccea
Denis Sokolov <denis@sokolov.cc>
2016-04-21 22:17:22 +0200

    Form to save the rule

--- toolbar/script.js
+++ toolbar/script.js
@@ -1,3 +1,9 @@
 chrome.tabs.insertCSS({
     code: '.ytp-progress-bar,.ytp-time-separator,.ytp-time-duration,.video-time{display:none!important}'
 });
+
+document.querySelector('.hide-automatically').addEventListener('click', function(){
+    document.querySelector('.hide-success').style.display = 'none';
+    document.querySelector('.add-a-rule').style.display = 'block';
+    document.querySelector('.add-a-rule input').focus();
+})
