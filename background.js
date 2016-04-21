1336bd22c1f4706ed05a104a6c9f3cce767a599d
Denis Sokolov <denis@sokolov.cc>
2016-04-21 23:17:28 +0200

    Hide duration automatically

--- background.js
+++ background.js
@@ -13,3 +13,19 @@ chrome.runtime.onInstalled.addListener(function() {
     ]);
   });
 });
+
+chrome.tabs.onUpdated.addListener(function(id){
+  chrome.tabs.get(id, function(tab){
+    if (!tab.title) return;
+    chrome.storage.sync.get('patterns', function(storage){
+      var patterns = Object.keys(storage.patterns || {});
+      var patternFound = patterns.some(function(pattern){
+          return tab.title.indexOf(pattern) > -1;
+      });
+      if (!patternFound) return;
+      chrome.tabs.insertCSS(id, {
+          code: '.ytp-progress-bar,.ytp-time-separator,.ytp-time-duration,.video-time{display:none!important}'
+      });
+    })
+  })
+})
