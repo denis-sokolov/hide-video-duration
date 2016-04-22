24fc639b93122a8d42b5604d7c6b877e381a8def
Denis Sokolov <denis@sokolov.cc>
2016-04-22 10:09:50 +0200

    Split background.js

--- src/hide-automatically.js
+++ src/hide-automatically.js
@@ -1,19 +1,4 @@
 
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    chrome.declarativeContent.onPageChanged.addRules([
-      {
-        conditions: [
-          new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: { hostEquals: 'www.youtube.com', pathPrefix: '/watch' }
-          })
-        ],
-        actions: [ new chrome.declarativeContent.ShowPageAction() ]
-      }
-    ]);
-  });
-});
-
 chrome.tabs.onUpdated.addListener(function(id){
   chrome.tabs.get(id, function(tab){
     if (!tab.title) return;
