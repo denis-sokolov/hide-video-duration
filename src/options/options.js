475b0e8d491acfc21cbb728b7578108d7c23d78b
Denis Sokolov <denis@sokolov.cc>
2016-04-23 22:26:00 +0200

    Options: allow to remove rules

--- src/options/options.js
+++ src/options/options.js
@@ -4,6 +4,18 @@ chrome.storage.sync.get('patterns', function(storage){
     patterns.forEach(function(pattern){
         var el = document.createElement('li');
         el.innerText = pattern;
+        var button = document.createElement('button');
+        button.innerText = chrome.i18n.getMessage('options_remove_rule');
+        button.style.marginLeft = '10px';
+        button.addEventListener('click', function(){
+            chrome.storage.sync.get('patterns', function(storage){
+                var val = (storage.patterns || {})
+                delete val[pattern];
+                chrome.storage.sync.set({ patterns: val });
+            });
+            el.remove();
+        });
+        el.appendChild(button);
         container.appendChild(el);
     });
 });
